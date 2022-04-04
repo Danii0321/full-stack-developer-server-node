e301827a812cee1b8eeafb499473d580ca8d54c2
Dani <dani10.rowe@gmail.com>
2022-04-04 16:02:38 -0400

    findusersbytype

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,8 +5,20 @@ const userController = (app) => {
  app.get('/api/users', findAllUsers);
 }
 
+const findUsersByType = (type) => {
+    const users_with_type = users.filter(function (users) {
+        return users.type == type});
+    return users_with_type;
+}
+
 const findAllUsers = (req, res) => {
+ const type = req.query.type;
+ if(type) {
+   res.json(findUsersByType(type));
+   return;
+ }
  res.json(users);
 }
+
 
 export default userController;
