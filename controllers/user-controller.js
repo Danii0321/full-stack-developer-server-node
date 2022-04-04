ddc1455fec1596e3b560fedba93e07f6f9206ffc
Dani <dani10.rowe@gmail.com>
2022-04-04 18:27:31 -0400

    adding postman functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,12 +3,20 @@ let users = people;
 
 const userController = (app) => {
  app.get('/api/users', findAllUsers);
+ app.get('/api/users/:uid', findUserById);
+ app.post('/api/users', createUser);
 }
 
 const findUsersByType = (type) => {
     const users_with_type = users.filter(function (users) {
         return users.type == type});
     return users_with_type;
+}
+
+const findUserById = (req, res) => {
+ const userId = req.params.uid;
+ const user = users.find(u => u._id === userId);
+ res.json(user);
 }
 
 const findAllUsers = (req, res) => {
@@ -18,6 +26,13 @@ const findAllUsers = (req, res) => {
    return;
  }
  res.json(users);
+}
+
+const createUser = (req, res) => {
+ const newUser = req.body;
+ newUser._id = (new Date()).getTime() + '';
+ users.push(newUser);
+ res.json(newUser);
 }
 
 
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import helloController from "./controllers/hello-controller.js";
+// import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 const app = express();
-helloController(app);
+app.use(express.json());
+// helloController(app);
 userController(app);
 app.listen(4000);
