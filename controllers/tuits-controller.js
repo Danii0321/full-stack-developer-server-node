cde24c44489e53cf4d547196970fc4f436aa5da2
Dani <dani10.rowe@gmail.com>
2022-04-06 20:30:11 -0400

    update

--- controllers/tuits-controller.js
+++ controllers/tuits-controller.js
@@ -18,6 +18,13 @@ const createTuit = (req, res) => {
  res.json(newTuit);
 }
 
+const updateTuit = (req, res) => {
+ const tuitdIdToUpdate = req.params.tid;
+ const updatedTuit = req.body;
+ tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+ res.sendStatus(200);
+}
+
 const findAllTuits = (req, res) =>
  res.json(tuits);
 const updateTuit = (req, res) => {}
