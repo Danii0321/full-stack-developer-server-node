7d4714976e4495010440ee900e19fddcc247f7bb
Dani <dani10.rowe@gmail.com>
2022-04-15 22:22:55 -0400

    connect to atlas

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 //var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/webdev');
-mongoose.connect('mongodb://0.0.0.0:27017/webdev');
+// mongoose.connect('mongodb://0.0.0.0:27017/webdev');
+const CONNECTION_STRING = "mongodb+srv://drowe:30627393Husky@cluster0.kpqzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
